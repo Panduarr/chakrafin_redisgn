e7968bb5a89568511882d1680f49a83440ab5fc3
Panduarr <pandurangamanikantap@gmail.com>
2026-02-13 10:17:20 +0530

    v0.0.0.05

--- src/app/(website)/(home)/page.jsx
+++ src/app/(website)/(home)/page.jsx
@@ -82,10 +82,10 @@ export const Banner = () => {
         ))}
 
         {/* ================= DARK OVERLAY ================= */}
-        <div className="absolute inset-0 bg-black/40 z-[1]" />
+        <div className="absolute inset-0" />
 
         {/* ================= TEXT CONTENT ================= */}
-        <div className="absolute inset-0 z-[2] flex items-center">
+        <div className="absolute inset-0  flex items-center">
           <div className="w-full">
             <div className="max-w-7xl mx-auto px-4 md:px-12">
               <h2 className="text-4xl md:text-5xl font-bold text-white leading-tight max-w-2xl">
